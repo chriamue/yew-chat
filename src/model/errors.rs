709a98cc9f8e70893c041bed36a48da6c433bbd0
Christian M <chriamue@gmail.com>
2024-09-23 12:34:24 +0200

    :goal_net: adds internal error

--- src/model/errors.rs
+++ src/model/errors.rs
@@ -4,12 +4,14 @@ use std::fmt;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum SendError {
     UnknownError,
+    InternalError(String),
 }
 
 impl fmt::Display for SendError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             SendError::UnknownError => write!(f, "Unknown error occurred"),
+            SendError::InternalError(e) => write!(f, "Internal error occurred: {}", e),
         }
     }
 }
@@ -19,12 +21,14 @@ impl std::error::Error for SendError {}
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum ReceiveError {
     UnknownError,
+    InternalError(String),
 }
 
 impl fmt::Display for ReceiveError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ReceiveError::UnknownError => write!(f, "Unknown error occurred"),
+            ReceiveError::InternalError(e) => write!(f, "Internal error occurred: {}", e),
         }
     }
 }
