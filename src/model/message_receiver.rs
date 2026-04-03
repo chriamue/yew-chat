f98dbda9745ff33686d65c48e09ed17c7f3a8dfa
Christian M <chriamue@gmail.com>
2026-04-03 13:20:23 +0200

    :arrow_up: upgrade dependencies

--- src/model/message_receiver.rs
+++ src/model/message_receiver.rs
@@ -1,15 +1,14 @@
 use super::Message;
 use super::ReceiveError;
-use async_trait::async_trait;
 
 #[cfg(not(feature = "yew"))]
-#[async_trait]
+#[allow(async_fn_in_trait)]
 pub trait MessageReceiver: Send {
     async fn receive_messages(&self, channel: &str) -> Result<Vec<Message>, ReceiveError>;
 }
 
 #[cfg(feature = "yew")]
-#[async_trait(?Send)]
+#[allow(async_fn_in_trait)]
 pub trait MessageReceiver {
     async fn receive_messages(&self, channel: &str) -> Result<Vec<Message>, ReceiveError>;
 }
--- src/model/message_sender.rs
+++ src/model/message_sender.rs
@@ -1,15 +1,14 @@
 use super::Message;
 use super::SendError;
-use async_trait::async_trait;
 
 #[cfg(not(feature = "yew"))]
-#[async_trait]
+#[allow(async_fn_in_trait)]
 pub trait MessageSender: Send {
     async fn send_message(&self, channel: &str, message: Message) -> Result<(), SendError>;
 }
 
 #[cfg(feature = "yew")]
-#[async_trait(?Send)]
+#[allow(async_fn_in_trait)]
 pub trait MessageSender {
     async fn send_message(&self, channel: &str, message: Message) -> Result<(), SendError>;
 }
